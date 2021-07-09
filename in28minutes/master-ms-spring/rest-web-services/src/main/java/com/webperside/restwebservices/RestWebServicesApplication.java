ea1f6f978e3c4cbc8403285e0c00c746c2f6cf5e
hamidsultanzadeh <sultanzade.06@mail.ru>
2021-07-09 12:22:48 +0400

    in28minutes initial

---
+++ in28minutes/master-ms-spring/rest-web-services/src/main/java/com/webperside/restwebservices/RestWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.webperside.restwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWebServicesApplication.class, args);
+    }
+
+}
---
+++ in28minutes/master-ms-spring/rest-web-services/src/test/java/com/webperside/restwebservices/RestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webperside.restwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
